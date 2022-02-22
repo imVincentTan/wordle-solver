85f3757187801084c1d541258fa3672b1ad75cb1
Vincent <vincent.tan.1998@gmail.com>
2022-02-21 21:33:08 -0800

    Edit tiny_solve_state to have 2^n sizes

--- test_tiny_solve_state.py
+++ test_tiny_solve_state.py
@@ -6,18 +6,18 @@ from tiny_solve_state import TinySolveState
 class TestTinySolveStateMethods(unittest.TestCase):
     def test_get_submission_hints(self):
         solve_state = TinySolveState()
-        result = solve_state.get_submission_hints('ab', 'bc')
-        self.assertEqual(result, '-y')
+        result = solve_state.get_submission_hints('aa', 'ab')
+        self.assertEqual(result, 'g-')
 
     def test_get_hint_pattern_to_frequency_map(self):
         solve_state = TinySolveState()
         result = solve_state.get_hint_pattern_to_frequency_map('ab')
-        self.assertEqual(result, {'g-': 2, 'gg': 1, 'yy': 1, '-g': 2, '-y': 1, 'y-': 1, '--': 1})
+        self.assertEqual(result, {'-g': 1, 'g-': 1, 'gg': 1, 'yy': 1})
 
     def test_get_expected_score(self):
         solve_state = TinySolveState()
         score = solve_state.get_expected_score('ab')
-        self.assertAlmostEqual(score, 2.72548055)
+        self.assertAlmostEqual(score, 2)
 
 if __name__ == '__main__':
     unittest.main()
--- tiny_solve_state.py
+++ tiny_solve_state.py
@@ -4,8 +4,8 @@ from wordlist import wordlist_small
 
 class TinySolveState(SolveState):
     def __init__(self) -> None:
-        self.possible_letters = {'a', 'b', 'c'}
+        self.possible_letters = {'a', 'b'}
         self.number_of_characters = 2
-        self.valid_input_words = ['aa', 'ab', 'ac', 'ba', 'bb', 'bc', 'ca', 'cb', 'cc']
-        self.possible_final_answers = ['aa', 'ab', 'ac', 'ba', 'bb', 'bc', 'ca', 'cb', 'cc']
+        self.valid_input_words = ['aa', 'ab',  'ba', 'bb']
+        self.possible_final_answers = ['aa', 'ab',  'ba', 'bb']
 
